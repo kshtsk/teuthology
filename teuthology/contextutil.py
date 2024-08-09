kshtsk/teuthology

commit ee43f87cfe88a8155566b662750d663c1d8742ff

safe_while: Retry forever when tries == -1
Signed-off-by: Zack Cerza <zack@redhat.com>

teuthology/contextutil.py
@@ -124,6 +124,8 @@ class safe_while(object):
         return msg
 
     def __call__(self):
+        if self.tries < 0:
+            return True
         self.counter += 1
         if self.counter == 1:
             return True

teuthology/test/test_contextutil.py
@@ -66,3 +66,9 @@ class TestSafeWhile(object):
                 pass
 
         assert True
+
+    def test_tries(self):
+        attempts = 0
+        with self.s_while(tries=-1, _sleeper=self.fake_sleep) as proceed:
+            while attempts < 100 and proceed():
+                attempts += 1

commit a6157b16adb5af464ca1f2e25a4f61584f501134

contextutil: still sleep between tries when tries is -1
This patch fixes an issue introduced within PR #1816,
that when tries are < 0 (e.g. -1) there is no sleep
occurred between tries.

It also fixes error message, so we repot correct
number of tries we've done now.

Fixes: ee43f87cfe88a8155566b662750d663c1d8742ff

Signed-off-by: Kyr Shatskyy <kyrylo.shatskyy@clyso.com>

teuthology/contextutil.py
@@ -115,20 +115,20 @@ class safe_while(object):
 
         msg = msg.format(
             action=self.action,
-            tries=self.counter,
+            tries=self.counter - 1,
             total=self.total_seconds,
         )
         return msg
 
     def __call__(self):
-        if self.tries < 0:
-            return True
         self.counter += 1
         if self.counter == 1:
             return True
+        def must_stop():
+            return self.tries > 0 and self.counter > self.tries
         if ((self.timeout > 0 and
              self.total_seconds >= self.timeout) or
-            (self.timeout == 0 and self.counter > self.tries)):
+            (self.timeout == 0 and must_stop())):
             error_msg = self._make_error_msg()
             if self._raise:
                 raise MaxWhileTries(error_msg)